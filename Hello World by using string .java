df37de9c11a0716d012352be1863764ad50cd134
Ferdouse H. Nowrin <84663223+nowrin50@users.noreply.github.com>
2024-07-06 16:28:18 +0600

    Hello World by using string .java

---
+++ Hello World by using string .java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main (String[] args) {
+     String message = "Hello World" + "!!";
+     System.out.println (message);
+    }
+}
+
+//endswith
+
+class Main {
+    public static void main (String[] args) {
+     String message = "Hello World" + "!!";
+     System.out.println (message.endsWith ("!!"));
+    }
+}
