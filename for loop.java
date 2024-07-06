c4e283a9e8e5c3ed7d33903784c40cef96f11862
Ferdouse H. Nowrin <84663223+nowrin50@users.noreply.github.com>
2024-07-06 16:32:39 +0600

    for loop.java

---
+++ for loop.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main {
+    public static void main (String[] args){
+ for (int i =0; i<5; i++) {  // The for-loop header for (int i = 0; i < 5; i++) initializes i to 0, checks if i is less than 5, and increments i by 1 after each iteration.  
+     System.out.println ("Hello World");
+ }
+    }
+}
