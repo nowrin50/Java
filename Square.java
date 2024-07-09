25b9b0b0e12eaee37dcf6d9c9adb996a1583cfce
Ferdouse H. Nowrin <84663223+nowrin50@users.noreply.github.com>
2024-07-09 08:19:35 +0600

    Square.java

---
+++ Square.java
@@ -0,0 +1,10 @@
+import java.util.* ;
+
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+        double a = sc.nextDouble() ;
+        double square = (a*a) ;
+        System.out.println (" the area" + square);
+    }
+}
