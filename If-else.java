5685fbb0bc08ba7cf2e78c9185f1ad565f052305
Ferdouse H. Nowrin <84663223+nowrin50@users.noreply.github.com>
2024-07-10 08:16:09 +0600

    If-else.java

---
+++ If-else.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main (String[] args) {
+        int a = 12;
+        int b = 22;
+        if (a>b) {
+            System.out.println ("A is bigger");
+        }
+        else {
+            System.out.println("B is bigger");
+        }
+    }
+}
